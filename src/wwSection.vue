440264851df96fad9c778a174f37ab0f841184c8
theintelligentguy <intelligentguy@yahoo.com>
2022-12-31 15:54:57 +0200

    finalized

--- src/wwSection.vue
+++ src/wwSection.vue
@@ -1,78 +1,77 @@
 <template>
-  <div>
-    <editor
-      id="test"
-      api-key="pco97szb51p38rzvnx6uos6t8hbly19imc8xgahac6yutt46"
-      v-model="tinymceText"
-      :init="{
-        height: 500,
-        menubar: true,
-        tinycomments_mode: 'embedded',
-        menubar: false,
-        toolbar_mode: 'sliding',
-        plugins: [
-          'a11ychecker',
-          'advlist',
-          'advcode',
-          'advtable',
-          'autolink',
-          'checklist',
-          'export',
-          'lists',
-          'link',
-          'image',
-          'charmap',
-          'preview',
-          'anchor',
-          'searchreplace',
-          'visualblocks',
-          'powerpaste',
-          'fullscreen',
-          'formatpainter',
-          'insertdatetime',
-          'media',
-          'table',
-          'help',
-          'wordcount',
-          'mergetags',
-        ],
+  <editor
+    id="test"
+    api-key="l5amtod88464kdqjbn8ysvh8o6xlhda9qvxpqtmpdo111nh4"
+    v-model="tinymceText"
+    :init="{
+      height: 500,
+      menubar: true,
+      tinycomments_mode: 'embedded',
+      menubar: false,
+      toolbar_mode: 'sliding',
+      branding: false,
+      plugins: [
+        'a11ychecker',
+        'advlist',
+        'advcode',
+        'advtable',
+        'autolink',
+        'checklist',
+        'export',
+        'lists',
+        'link',
+        'image',
+        'charmap',
+        'preview',
+        'anchor',
+        'searchreplace',
+        'visualblocks',
+        'powerpaste',
+        'fullscreen',
+        'formatpainter',
+        'insertdatetime',
+        'media',
+        'table',
+        'help',
+        'wordcount',
+        'mergetags',
+      ],
 
-        toolbar:
-          'undo redo | fontfamily fontsize forecolor|  mergetags| bold italic underline | link image| align lineheight | checklist numlist bullist indent outdent',
-        mergetags_list: [
-          {
-            value: 'placeholder@gmail.com',
-            title: 'Email',
-          },
-          {
-            value: 'firstName',
-            title: 'First Name',
-          },
-          {
-            value: '2pac',
-            title: 'Last Name',
-          },
-          {
-            value: 'Mobile',
-            title: 'Mobile',
-          },
-          {
-            value: 'linkedinUrl',
-            title: 'LinkedIn URL',
-          },
-          {
-            value: 'companyName',
-            title: 'Company Name',
-          },
-          {
-            value: 'Title',
-            title: 'Title',
-          },
-        ],
-      }"
-      initial-value="Hello there"
-    />
-  </div>
+      toolbar:
+        'undo redo | fontfamily fontsize forecolor|  mergetags| bold italic underline | link image| align lineheight | checklist numlist bullist indent outdent',
+      mergetags_list: [
+        {
+          value: 'placeholder@gmail.com',
+          title: 'Email',
+        },
+        {
+          value: 'firstName',
+          title: 'First Name',
+        },
+        {
+          value: '2pac',
+          title: 'Last Name',
+        },
+        {
+          value: 'Mobile',
+          title: 'Mobile',
+        },
+        {
+          value: 'linkedinUrl',
+          title: 'LinkedIn URL',
+        },
+        {
+          value: 'companyName',
+          title: 'Company Name',
+        },
+        {
+          value: 'Title',
+          title: 'Title',
+        },
+      ],
+    }"
+    initial-value="Hello there"
+  />
 </template>
 
 <script>
