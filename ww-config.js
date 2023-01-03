11034c0f4bf0051cd6d0b695f638f206d760bdd6
theintelligentguy <intelligentguy@yahoo.com>
2023-01-03 06:07:25 +0200

    tiny fixes

--- ww-config.js
+++ ww-config.js
@@ -1,23 +1,8 @@
 export default {
   editor: {
     label: {
-      en: "My__Section",
+      en: "My__Element",
     },
   },
-  properties: {
-    textFromTiny: {
-      label: {
-        en: "text",
-      },
-      type: "text",
-      defaultValue: "Insert your text here",
-      bindable: true,
-    },
-    labelComponent: {
-      type: "ww-text",
-      defaultValue: {
-        isWwObject: true,
-      },
-    },
-  },
+  properties: {},
 };
